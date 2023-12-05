a649f308502537ca74e972ed1901324a6078bcd4
Min <minjung10402@gmail.com>
2023-12-04 19:28:06 -0800

    update

--- client.py
+++ client.py
@@ -4,6 +4,9 @@ import json as pickle
 import time
 import socket
 import threading
+import ssl
+
+CERTIFICATE_PATH = "./crypto/certificate.pem"
 
 ################################
 # GENERAL USE CASE:
@@ -36,7 +39,6 @@ class TCP:
 class LoginScreen:
     def __init__(self):
        
-        
         # Create window
         self.root = Tk()
         self.root.title("The Securest Chat App")
@@ -45,35 +47,40 @@ class LoginScreen:
         self.root.configure(background=dark_grey)
         
         # Wdiget
+        # Login Label
         self.login_label = Label(self.root,text="Log In", font=font_lrg, bg=dark_grey, fg=aqua_blue)
+        # User Label and Entry
         self.user_label = Label(self.root, text="Username: ", font=font_med, bg=dark_grey, fg=white)
         self.user_entry = Entry(self.root)
-        self.password_label = Label(self.root, text="Password: ", font=font_med, bg=dark_grey, fg=white)
-        self.password_entry = Entry(self.root, show="*")
+        # IP Address Label and Entry
+        self.address_label = Label(self.root, text="Chatroom IP: ", font=font_med, bg=dark_grey, fg=white)
+        self.address_entry = Entry(self.root, show="*")
+        # Start Connection Button
         self.start_button = Button(self.root, text="Connect", font=font_med, bg=aqua_blue, fg=black, command=self.connect)
     
         # Wdiget Placement
+        # Login
         self.login_label.place(relx=.5, rely=.2,anchor= CENTER)
+        # User Label and Entry
         self.user_label.place(relx=.3, rely=.3,anchor= CENTER)
         self.user_entry.place(relx=.6, rely=.3,anchor= CENTER)
-        self.password_label.place(relx=.3, rely=.35,anchor= CENTER)
-        self.password_entry.place(relx=.6, rely=.35,anchor= CENTER)
+        # IP Address Label and Entry
+        self.address_label.place(relx=.3, rely=.35,anchor= CENTER)
+        self.address_entry.place(relx=.6, rely=.35,anchor= CENTER)
+        # Connect Button
         self.start_button.place(relx=.5, rely=.45,anchor= CENTER)
    
     def connect(self):
-        username = self.user_entry.get()
-        password = self.password_entry.get()
-        
-        # Check authenitciation here
-        
-        
+        self.username = self.user_entry.get()
+        self.address = self.address_entry.get()
+
         # Close the login screen
         self.root.destroy()
         
         # Go to main screen
-        main_screen = MainChatScreen(username)
+        main_screen = MainChatScreen(self.username, self.address)
         main_screen.run()
-
+        
     def run(self):
         self.root.mainloop()
         
@@ -82,7 +89,8 @@ class LoginScreen:
         
     
 class MainChatScreen:
-    def __init__(self, username):
+    def __init__(self, username, address):
+       
         # Create window
         self.root = Tk()
         self.root.title("The Securest Chat App")
@@ -91,17 +99,27 @@ class MainChatScreen:
         self.root.configure(background=dark_grey)
         
         # Widget 
+        # Username
         self.username = username
-        self.username_label = Label(self.root, text=self.username)
-        self.username_label.pack(pady=30)
+        self.username_label = Label(self.root, text="Username:" + self.username)
+        self.username_label.pack(pady=20)
+        # Password 
+        self.address = address
+        self.address_label = Label(self.root, text="Address: " + self.address)
+        self.address_label.pack()
         # msg feed
         # msg input
-        self.msg = Text(self.root, bg=white, fg=black, font=font_sm, width=59)
-        self.msg_entry = Entry(self.root, bg=white, fg=black, font=font_sm, width=52)
+        self.msg = Text(self.root, text="Message Feed", bg=aqua_blue, fg=black, font=font_sm, height=35)
+        self.msg.pack(pady=(20,0))
+        self.msg_entry = Entry(self.root, bg=white, fg=black, font=font_sm, width=50)
+        self.msg_entry.pack(side=LEFT)
         # send msg btn
-        self.send_btn = Button(self.root, text="Send", font=font_sm_bold, bg=aqua_blue, fg=black)
+        # self.send_btn = Button(self.root, text="Send", font=font_sm_bold, bg=aqua_blue, fg=black, width=10, command=self.send_message)
+ 
         # side bar with chatroom name
         
+        # Accept Connection
+        self.accept_connection()
     
         #create label to update
         self.data = "waiting..."
@@ -109,10 +127,13 @@ class MainChatScreen:
         self.label.pack(pady=40)
         self.counter = 0
         
+        self.nickname = ""
+        
         # Widget Placement
-        self.msg.place(relx=.5, rely=.635,anchor= CENTER)
-        self.msg_entry.place(relx=.455, rely=.90,anchor= CENTER)
-        self.send_btn.place(relx=.855, rely=.90,anchor= CENTER)
+        # self.msg.pack(pady=30)
+        # self.msg.place(relx=.5, rely=.635,anchor= CENTER)
+        # self.msg_entry.place(relx=.455, rely=.90,anchor= CENTER)
+        # self.send_btn.place(relx=.855, rely=.90,anchor= CENTER)
         
 
     def run(self):
@@ -131,9 +152,31 @@ class MainChatScreen:
         msg_serial = pickle.dumps(tcp_message)
 
         self.con.sendall(msg_serial.encode("utf-8"))
-
-    def recv_message(self):
-        pass
+        
+    def recv_message(self, client):
+        while True:
+            try:
+                message = client.recv(1024).decode('ascii')
+                if message == 'NICK':
+                    client.send(self.nickname.encode('ascii'))
+                else:
+                    print(message)
+            except:
+                print('An error occured')
+                client.close()
+                break
+            
+    def write_message(self, client):
+        message = 0
+        while True:
+            #TODO get input from user
+            #message = f'{self.nickname}: {input("")}'
+            time.sleep(5)
+            
+            #send_msg = self.send_message(str(message+1))
+            send_msg = f"{self.username}: {str(message)}"
+            client.send(send_msg.encode('ascii'))
+            message += 1
 
     def update_label(self):
         self.data.set(f"{self.counter}")
@@ -142,20 +185,33 @@ class MainChatScreen:
     def start_thread(self):
         threading.Thread(target=self.accept_connection).start()
     
-    def accept_connection(self):
-        #get username TODO: Once username box is implemented, we should get text from it. For now we set a static value
-        self.username = "temp_name"
-
-        #create socket
+    def accept_connection(self, address):
+        self.port = 9001
+        
+        # Create ss1 context
+        print("creating ss1")
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        context.load_verify_locations(CERTIFICATE_PATH)
+        context.check_hostname = False
+        
+        # Create socket
+        print("creating socket")
         self.con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        #connect to the server
-        self.con.connect(("127.0.0.1", 9001))
-
-        #shake hands TODO: add encryption to connection when it is established here
-        for x in range(10):
+        # Secure socket
+        print("secure socket")
+        self.sec_con = context.wrap_socket(self.con)
+        
+        # Connect to the server
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((address, self.port))
+        for i in range(10):
+            message = f"message {i}"
+            s.sendall(message.encode("utf-8"))
             time.sleep(1)
-            self.send_message(f"hello {x}")
+        s.close()
+        
+
 
     def connect(self, host, port):
         #definte connection information
@@ -170,3 +226,4 @@ class MainChatScreen:
 if __name__ == "__main__":
     gui = LoginScreen()
     gui.run()
+
--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 import socket
+import threading
 import time
 import json as pickle
+import ssl
+
+PRIV_KEY_PATH = "./crypto/private.key"
+CERTIFICATE_PATH = "./crypto/certificate.pem"
 
 class connection:
     def __init__(self, con, addr):
         self.con = con
         self.addr = addr
-
-        self.keep_alive = True
     
     def decode_msg(self, data):
-        #TODO: when encryption is implemented, decrypt data prior to unloading
-
         #deserialize data
         decoded_data = data.decode()
         print("decoded: ", decoded_data)
@@ -20,35 +21,62 @@ class connection:
 
         return message_dict
     
-    def keep_listening(self):
-        with self.con:
-            print("connected")
-            while self.keep_alive:
-                data = self.con.recv(1024)
-                data = self.decode_msg(data)
-
-                print(data)
-
-                #TODO: handle message rather than just printing it (aka send it to all other clients)
+   
 class server:
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.clients = []
+        self.nicknames =[]
+
+    def broadcast(self, message):
+        for client in self.clients:
+            client.send(message)
+
+    def handle(self, client):
+        while True:
+            try:
+                message = client.recv(1024)
+                # decrypt message
+                self.broadcast(message)
+            except:
+                index = self.clients.index(client)
+                self.clients.remove(client)
+                client.close()
+                nickname = self.nicknames[index]
+                self.broadcast(f'{nickname} left the chat!'.encode('ascii'))
+                self.nicknames.remove(nickname)
+                break
 
     def runserver(self):
+        #create ssl context
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.load_cert_chain(certfile=CERTIFICATE_PATH, keyfile=PRIV_KEY_PATH)
+
         #create socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((self.host, self.port))
-
-        #listen for new connections TODO: implement 10+ threaded chat clients rather than just one.
         s.listen()
-        con, addr = s.accept()
-        
-        #maintain list of connections (For now just one)
-        cons = []
-        cons.append(connection(con, addr))
-        cons[-1].keep_listening() #TODO: add threading to this            
+        print(f'Listening on {(self.host, self.port)}...')
+
+        while True:
+            client, address = s.accept()
+            client = context.wrap_socket(client, server_side=True)
+            print(f'Connected with {str(address)}')
+
+            client.send('NICK'.encode('ascii'))
+            nickname = client.recv(1024).decode('ascii')
+            self.nicknames.append(nickname)
+            self.clients.append(client)
+
+            print(f'Nickname of the client is {nickname}!')
+            self.broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+            client.send('\nConnected to the server!'.encode('ascii'))
+
+            thread = threading.Thread(target=self.handle, args=(client,))
+            thread.daemon = True
+            thread.start()          
 
 if __name__  == "__main__":
-    serv = server("127.0.0.1")
+    serv = server("127.0.0.1", 9001)
     serv.runserver()
